71fa1768019df8c8bd4d9f87a5dda51f17101061
Samvit Prakash <samvitprakash2004@gmail.com>
2026-09-21 22:35:21 +0200

    Refactor codebase for three executables

---
+++ src/phantom_haunt/phantomHaunt.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello, World! From the Phantom Haunt" << endl;
+
+    return 0;
+}
---
+++ src/phantomd/phantomd.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello, World! From the Phantom Daemon" << endl;
+
+    return 0;
+}
